36afd883379697ce70fcad6562eab3c35d7900b4
yosuke omori <taro.kirameki@techacademy.jp>
2018-07-02 12:34:20 +0000

    Finish user edit, update, index, and destroy actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,19 +2,30 @@ class SessionsController < ApplicationController
 
   def new
   end
-
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       log_in user
-      #remember user
       params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-      redirect_to user      #user_url(user)と同じ
+      redirect_back_or user
     else
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
     end
   end
+
+  # def create
+  #   user = User.find_by(email: params[:session][:email].downcase)
+  #   if user && user.authenticate(params[:session][:password])
+  #     log_in user
+  #     #remember user
+  #     params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+  #     redirect_to user      #user_url(user)と同じ
+  #   else
+  #     flash.now[:danger] = 'Invalid email/password combination'
+  #     render 'new'
+  #   end
+  # end
 
  def destroy
     log_out if logged_in?
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,6 +1,7 @@
 require 'test_helper'
 
-class UserTest < ActiveSupport::TestCase
+#class UserTest < ActiveSupport::TestCase
+class UsersControllerTest < ActionDispatch::IntegrationTest
 
   def setup
     @user = User.new(name: "Example User", email: "user@example.com")
@@ -10,6 +11,11 @@ class UserTest < ActiveSupport::TestCase
                      password: "foobar", password_confirmation: "foobar")
   end
 
+  def setup
+    @user = users(:michael)
+    @other_user = users(:archer)
+  end
+  
   test "should be valid" do
     assert @user.valid?
   end
@@ -60,5 +66,55 @@ class UserTest < ActiveSupport::TestCase
     @user.password = @user.password_confirmation = "a" * 5
     assert_not @user.valid?
   end
+
+  test "should redirect edit when not logged in" do
+    #get edit_user_path(@user)
+    get edit_user_path(@user)
+    assert_not flash.empty?
+    assert_redirected_to login_url
+  end
+
+  test "should redirect update when not logged in" do
+    patch user_path(@user), params: { user: { name: @user.name,
+                                              email: @user.email } }
+    assert_not flash.empty?
+    assert_redirected_to login_url
+  end
+
+ test "should redirect edit when logged in as wrong user" do
+    log_in_as(@other_user)
+    get edit_user_path(@user)
+    assert flash.empty?
+    assert_redirected_to root_url
+  end
+
+  test "should redirect update when logged in as wrong user" do
+    log_in_as(@other_user)
+    patch user_path(@user), params: { user: { name: @user.name,
+                                              email: @user.email } }
+    assert flash.empty?
+    assert_redirected_to root_url
+  end
+
+  test "should redirect index when not logged in" do
+    get users_path
+    assert_redirected_to login_url
+  end
+  
+  test "should redirect destroy when not logged in" do
+    assert_no_difference 'User.count' do
+      delete user_path(@user)
+    end
+    assert_redirected_to login_url
+  end
+
+  test "should redirect destroy when logged in as a non-admin" do
+    log_in_as(@other_user)
+    assert_no_difference 'User.count' do
+      delete user_path(@user)
+    end
+    assert_redirected_to root_url
+  end
+
 
 end
